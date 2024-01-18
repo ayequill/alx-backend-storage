a38fb3808a838fe955dee9ff803a79f17e29badc
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-18 21:56:37 +0000

    A script that create a users table with enum values

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- A script that creates a table
+CREATE TABLE IF NOT EXISTS users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+    );
