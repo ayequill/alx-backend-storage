95b12a094cbc88fa293341606c192828da9c7867
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-18 23:25:30 +0000

    refactored

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
 -- Create a table named users
-CREATE TABLE IF NOT EXISTS users(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users
+(
+    id    INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-    );
+    name  VARCHAR(255)
+);
