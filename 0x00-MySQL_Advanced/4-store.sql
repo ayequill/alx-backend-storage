e821c6a095b91c50bbb0cc1eb4ac153a1895089c
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-18 23:28:02 +0000

    creates a trigger to handle an event where a new order is added

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,9 @@
+-- Creates a trigger to handle an event where a new order is added
+-- the trigger decreases the quantity of the order
+CREATE TRIGGER after_insert_order
+    AFTER INSERT ON orders FOR EACH ROW
+    BEGIN
+        UPDATE items
+        SET quantity = quantity - 1
+        WHERE name = NEW.item_name;
+    END;
