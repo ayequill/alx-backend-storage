f98b1b8846eec71546761d27769e6e6dc4505185
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-18 23:37:54 +0000

    refactored

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -4,6 +4,6 @@ CREATE TRIGGER after_insert_order
     AFTER INSERT ON orders FOR EACH ROW
     BEGIN
         UPDATE items
-        SET quantity = quantity - 1
+        SET quantity = quantity - NEW.number
         WHERE name = NEW.item_name;
     END;
