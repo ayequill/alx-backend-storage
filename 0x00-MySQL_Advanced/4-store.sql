f91bc071eb0817c9c7102faeeefe8d5a43479ba7
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-19 00:13:51 +0000

    refactored

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,9 +1,14 @@
 -- Creates a trigger to handle an event where a new order is added
 -- the trigger decreases the quantity of the order
+DELIMITER $$
 CREATE TRIGGER after_insert_order
-    AFTER INSERT ON orders FOR EACH ROW
-    BEGIN
-        UPDATE items
-        SET quantity = quantity - NEW.number
-        WHERE name = NEW.item_name;
-    END;
+    AFTER INSERT
+    ON orders
+    FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END;
+$$
+DELIMITER ;
