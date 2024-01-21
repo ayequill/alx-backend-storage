74f648237be3c5fec813b5dd2c3cffc0c9fa6409
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-21 00:43:20 +0000

    Computes weighted average

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,16 @@
+-- A stored procedure to compute weighted average
+-- obtained by a student
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(userID INT)
+BEGIN
+    DECLARE avg FLOAT;
+    SELECT SUM(score * weight) / SUM(weight)
+    INTO avg
+    FROM corrections
+             JOIN projects p ON p.id = project_id
+    WHERE userID = user_id;
+
+    UPDATE users
+    SET average_score = avg
+    WHERE id = userID;
+END;
