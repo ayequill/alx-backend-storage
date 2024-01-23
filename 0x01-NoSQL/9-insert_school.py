0474db19f369c854904a13800c6d2536d7c29112
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-23 10:03:58 +0000

    refactored
    Signed-off-by: Siaw A. Nicholas <siawnic.dev@gmail.com>

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """module that inserts a doc in school"""
-from pymongo.collection import Collection
 
 
-def insert_school(mongo_collection: Collection, **kwargs):
+def insert_school(mongo_collection, **kwargs):
     """ Inserts a doc in collection """
     inserted = mongo_collection.insert_one(kwargs)
     return inserted.inserted_id
