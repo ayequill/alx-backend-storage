29acb9c7451ce55c6df329b1f60490821ce64095
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-23 22:47:25 +0000

    Prints out nginx logs
    Signed-off-by: Siaw A. Nicholas <siawnic.dev@gmail.com>

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""queries a collection and prints the results"""
+from pymongo import MongoClient
+from pymongo.collection import Collection
+from pymongo.database import Database
+
+
+def connect_db():
+    db_client: MongoClient = MongoClient('mongodb://127.0.0.1:27017')
+    logs_db: Database = db_client.logs
+    nginx_col: Collection = logs_db.nginx
+    return nginx_col
+
+
+def return_str(method: str, count: int):
+    """returns a string"""
+    return f'\tmethod {method}: {count}'
+
+
+def print_results(methods):
+    """ Print results from logs """
+    db = connect_db()
+    print(db.count_documents({}), 'logs', '\nMethods:')
+    for method in methods:
+        to_print = db.count_documents({'method': method})
+        print(return_str(method, to_print))
+    print(db.count_documents(
+        {'method': 'GET', '$and': [{'path': '/status'}]}), 'status check'
+    )
+
+
+if __name__ == "__main__":
+    print_results(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
