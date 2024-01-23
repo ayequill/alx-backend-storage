7eb175295a863bc2862a26c3c0d19e9958aa4cd2
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-23 09:55:49 +0000

    refactored
    Signed-off-by: Siaw A. Nicholas <siawnic.dev@gmail.com>

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 """module finds all docs in a collection"""
-from pymongo.collection import Collection
-from pymongo.cursor import Cursor
-from typing import List
 
 
-def list_all(mongo_collection: Collection) -> List:
+def list_all(mongo_collection):
     """ Queries and returns all docs in a collection"""
-    cursor: Cursor = mongo_collection.find()
+    cursor = mongo_collection.find()
     return list(cursor)
