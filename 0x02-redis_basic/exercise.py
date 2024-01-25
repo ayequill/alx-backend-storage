aa4baee392f382e183f9bfd2ab0f1f9baab18f84
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-25 01:45:23 +0000

    store the history of inputs and outputs for a particular function.
    Signed-off-by: Siaw A. Nicholas <siawnic.dev@gmail.com>

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,6 +18,21 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """ store the history of inputs and outputs for a particular function. """
+    inputs = f"{method.__qualname__}:inputs"
+    outputs = f"{method.__qualname__}:outputs"
+
+    @wraps(method)
+    def wrapper(self: Any, *args, **kwd) -> Any:
+        self._redis.rpush(inputs, str(args))
+        output = method(self, str(args))
+        self._redis.rpush(outputs, output)
+        return output
+
+    return wrapper
+
+
 class Cache:
     """ Cache Class for data persistence """
 
@@ -26,6 +41,7 @@ class Cache:
         self._redis = Redis()
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Stores and returns a key """
