278dfce8672360e50e1daf58c78487278c6947cf
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-26 23:44:06 +0000

    Refactored
    Signed-off-by: Siaw A. Nicholas <siawnic.dev@gmail.com>

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,10 +5,12 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     db_client = MongoClient('mongodb://127.0.0.1:27017')
     logs_db = db_client.logs.nginx
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print(logs_db.count_documents({}), 'logs', '\nMethods:')
-    for method in methods:
-        print(f"\tmethod {method}:", logs_db.count_documents({"method": method}))
-    print(logs_db.count_documents(
-        {'method': 'GET', 'path': '/status'}), 'status check'
-    )
+    print("\tmethod GET:", logs_db.count_documents({"method": "GET"}))
+    print("\tmethod POST:", logs_db.count_documents({"method": "POST"}))
+    print("\tmethod PUT:", logs_db.count_documents({"method": "PUT"}))
+    print("\tmethod PATCH:", logs_db.count_documents({"method": "PATCH"}))
+    print("\tmethod DELETE:", logs_db.count_documents({"method": "DELETE"}))
+    print(logs_db.count_documents({
+        "method": "GET", "path": "/status"
+    }), "status check")
