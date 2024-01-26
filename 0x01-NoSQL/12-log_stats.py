2e22857007c8268264a920376164f592f1cc216c
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-26 23:33:36 +0000

    Refactored
    Signed-off-by: Siaw A. Nicholas <siawnic.dev@gmail.com>

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,29 +3,12 @@
 from pymongo import MongoClient
 
 if __name__ == "__main__":
-    def connect_db():
-        """Connect DB"""
-        db_client = MongoClient('mongodb://127.0.0.1:27017')
-        logs_db = db_client.logs
-        nginx_col = logs_db.nginx
-        return nginx_col
-
-
-    def return_str(method, count):
-        """returns a string"""
-        return f'\tmethod {method}: {count}'
-
-
-    def print_results(methods):
-        """ Print results from logs """
-        db = connect_db()
-        print(db.count_documents({}), 'logs', '\nMethods:')
-        for method in methods:
-            to_print = db.count_documents({'method': method})
-            print(return_str(method, to_print))
-        print(db.count_documents(
-            {'method': 'GET', 'path': '/status'}), 'status check'
-        )
-
-
-    print_results(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
+    db_client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_db = db_client.logs.nginx
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print(logs_db.count_documents({}), 'logs', '\nMethods:')
+    for method in methods:
+        print(f"\tmethod {method}:", logs_db.count_documents({"method": method}))
+    print(logs_db.count_documents(
+        {'method': 'GET', 'path': '/status'}), 'status check'
+    )
